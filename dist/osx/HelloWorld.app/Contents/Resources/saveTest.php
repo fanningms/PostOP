fd495c182f1c094f9fe10fab30f1266663ac08b4
Miller Fanning <fanningms@g.cofc.edu>
2015-03-24 11:57:35 -0400

    TideSDK framework

---
+++ dist/osx/HelloWorld.app/Contents/Resources/saveTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admintest
+ * Date: 3/23/15
+ * Time: 6:17 PM
+ */
+$firstName = $_GET['first_name'];
+$lastName = $_GET['last_name'];
+?>
+
+<script>
+    alert("Alert");
+</script>

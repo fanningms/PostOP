2a2955780d30210a916c0560d69220e072f79c63
Miller Fanning <fanningms@g.cofc.edu>
2015-03-24 13:56:06 -0400

    Facebook login and partial save

---
+++ dist/osx/PostOP.app/Contents/Resources/saveData.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admintest
+ * Date: 3/24/15
+ * Time: 12:34 PM
+ */
+
+$firstName = $_GET['first_name'];
+$lastName = $_GET['last_name'];
+
+echo $firstName  ." " . $lastName;
---
+++ dist/osx/PostOP.app/Contents/Resources/test.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admintest
+ * Date: 3/23/15
+ * Time: 3:59 PM
+ */
+
+$lastName = $_GET['first_name'];
+$firstName = $_GET['last_name'];
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+    <title>Hello World</title>
+    <style type="text/css">
+        body {background: #fff;}
+    </style>
+</head>
+<body>
+
+<h1><?php echo("PHP preprocessed output!");?></h1>
+<script type="text/javascript" src="app.js"></script>
+</body>
+</html>
